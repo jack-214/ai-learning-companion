75899b51660ef06f0e36dfe48796b376e6b93919
Jack Ho <jack.ho12345@gmail.com>
2025-08-19 22:16:45 +0100

    feat: implement CTA component with layout and styling

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section className="bg-cta text-white rounded-4xl px-7 py-10 flex flex-col items-center text-center gap-5 w-1/3 max-lg:w-1/2 max-md:w-full">
+      <div className="bg-cta-gold rounded-4xl px-3 py-1.5 text-black">
+        Start learning your way.
+      </div>
+      <h2 className="text-3xl font-bold">
+        Build and Personalise Learning Companion
+      </h2>
+      <p>
+        Pick a name, subject, voice & personality - and start learning through
+        voice conversations that feel natrual and fun.
+      </p>
+      <Image src="/images/cta.svg" alt="cta" width={362} height={232} />
+      <button className="bg-primary text-white rounded-xl cursor-pointer px-4 py-2 flex items-center gap-2">
+        <Image src="/icons/plus.svg" alt="plus" width={12} height={12} />
+        <Link href="/companions/new">
+          <p>Build a New Companion</p>
+        </Link>
+      </button>
+    </section>
+  );
 };
 
 export default CTA;
